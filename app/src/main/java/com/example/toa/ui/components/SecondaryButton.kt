4fdaf13d0e6b10f7765adfb8b4fd1169d8e1cfbc
Hrishikesh <hrishikesh.dharshan@citizensbank.com>
2022-01-20 00:00:43 -0500

    Added Secondary Button

--- app/src/main/java/com/example/toa/ui/components/SecondaryButton.kt
+++ app/src/main/java/com/example/toa/ui/components/SecondaryButton.kt
@@ -3,13 +3,11 @@ package com.example.toa.ui.components
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults.buttonColors
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.toUpperCase
@@ -19,19 +17,13 @@ import com.example.toa.ui.theme.ButtonShape
 import com.example.toa.ui.theme.TOATheme
 
 @Composable
-fun PrimaryButton(
+fun SecondaryButton(
     modifier: Modifier = Modifier,
     text: String,
-    backgroundColor: Color = MaterialTheme.colors.primary,
     onClick: () -> Unit = {}
 ) {
-    val buttonColors = buttonColors(
-        backgroundColor = backgroundColor
-    )
-
-    Button(
+    TextButton(
         onClick = onClick,
-        colors = buttonColors,
         shape = ButtonShape,
         modifier = modifier
             .height(dimensionResource(id = R.dimen.button_height))
@@ -52,12 +44,13 @@ fun PrimaryButton(
     uiMode = Configuration.UI_MODE_NIGHT_NO,
 )
 @Composable
-private fun PrimaryButtonPreview() {
+private fun SecondaryButtonPreview() {
     TOATheme {
-        PrimaryButton(
-            text = "Default",
-            onClick = {},
-
-        )
+        Surface {
+            SecondaryButton(
+                text = "Default",
+                onClick = {},
+            )
+        }
     }
 }
